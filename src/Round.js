f82efde17f3b22f6039893f48fa432c22d903558
Marika <marikashanahan@gmail.com>
2021-02-03 22:36:07 -0700

    Finish test draft for Round-test.js

--- src/Round.js
+++ src/Round.js
@@ -6,6 +6,9 @@ class Round {
 
 module.exports = Round;
 
+// Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
+
+// Your Round class should meet the following requirements:
 // returnCurrentCard: method that returns the current card being played
 // takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
 
@@ -14,5 +17,6 @@ module.exports = Round;
 // The next card becomes current card
 // Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
 // Feedback is returned regarding whether the guess is incorrect or correct
+
 // calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
 // endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,6 +1,8 @@
 const chai = require('chai')
 const expect = chai.expect
+const Deck = require('../src/Deck')
 const Round = require('../src/Round')
+const Turn = require('../src/Turn')
 
 describe('Round Class', () => {
   it('should be a function', () => {
@@ -12,15 +14,103 @@ describe('Round Class', () => {
     expect(round).to.be.an.instanceOf(Round)
   })
 
-  it('should increment turn count regardless of guess accuracy')
+  it('should be able to have a deck', () => {
+    const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+    const deck = new Deck([card32])
+    const round = new Round(deck)
+    expect(round.deck).to.deep.equal(deck)
+  })
 
-  it('should change the current card to the next card in the deck once a guess is made')
+  it('should store the deck as an instance of the Deck class', () => {
+    const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+    const deck = new Deck([card32])
+    const round = new Round(deck)
+    expect(round.deck).to.be.an.instanceOf(Deck)
+  })
 
-  it('should be able to store incorrect guesses')
+  it('should be able to have any deck', () => {
+    const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+    const diffDeck = new Deck ([card78])
+    const round = new Round (diffDeck)
+    expect(round.deck).to.deep.equal(diffDeck)
+  })
 
-  it('should be able to give feedback')
+  it('should be able to return the current card', () => {
+    const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+    const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+    const deck = new Deck([card32, card78])
+    const round = new Round(deck)
+    expect(round.returnCurrentCard()).to.deep.equal(card32)
+  })
 
-  it('should be able to calculate the percentage of correct guesses')
+  describe('takeTurn method', () => {
+    it('should create an instance of the Turn class when user makes a guess', () => {
+      const round = new Round ()
+      round.takeTurn()
+      expect(round.takeTurn()).to.be.an.instanceOf(Turn)
+    })
+  
+    it('should increment turn count regardless of guess accuracy', () => {
+      const round = new Round ()
+      expect(round.takeTurn()).to.increase(round.turnCount).by(1)
+    })
+  
+    it('should change the current card to the next card in the deck once a guess is made', () => {
+      const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+      const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+      const deck = new Deck ([card32, card78])
+      const round = new Round (deck)
+      round.takeTurn()
+      expect(round.returnCurrentCard()).to.deep.equal(card78)
+    })
+  
+    it('should be able to store incorrect guesses', () => {
+      const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+      const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+      const deck = new Deck ([card32, card78])
+      const round = new Round (deck)
+      round.takeTurn('2013')
+      expect(round.incorrectGuesses).to.deep.equal([])
+      round.takeTurn('Living Coral')
+      expect(round.incorrectGuesses).to.deep.equal([card78.id])
+    })
 
-  it('should be able to end the round')
+    it('should store incorrect guesses as an array', () => {
+      const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+      const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+      const deck = new Deck ([card32, card78])
+      const round = new Round (deck)
+      round.takeTurn('2013')
+      expect(round.incorrectGuesses).to.be.an('array')
+    })
+  
+    it('should be able to give accurate feedback', () => {
+      const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+      const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+      const deck = new Deck ([card32, card78])
+      const round = new Round (deck)
+      expect(round.takeTurn('2013')).to.deep.equal('correct!')
+      expect(round.takeTurn('2014')).to.deep.equal('incorrect!')
+    })
+  })
+
+  it('should be able to calculate the percentage of correct guesses', () => {
+    const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+    const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+    const deck = new Deck ([card32, card78])
+    const round = new Round (deck)
+    round.takeTurn('2013')
+    round.takeTurn('Living Coral')
+    expect(round.calculatePercentCorrect()).to.deep.equal(50)
+  })
+
+  it('should be able to end the round with a dynamic message', () => {
+    const card32 = new Card (32, 'What year did BTS debut?', ['2012', '2013', '2014'], '2013')
+    const card78 = new Card (78, 'What is the Pantone Color of the Year for 2021?', ['Serenity Blue', 'Living Coral', 'Ultimate Grey'], 'Ultimate Grey')
+    const deck = new Deck ([card32, card78])
+    const round = new Round (deck)
+    round.takeTurn('2013')
+    round.takeTurn('Living Coral')
+    expect(round.endRound()).to.deep.equal("** Round over! ** You answered 50% of the questions correctly!")
+  })
 })
