649bd5d1b4d5784550664547958b88e125a64f99
Marika <marikashanahan@gmail.com>
2021-02-04 19:03:05 -0700

    Card-test.js linting

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const http = require('http');
 let app = http.createServer();
-// const data = require('./src/data');
-// const prototypeQuestions = data.prototypeData;
-const util = require('./src/util')
+// const util = require('./src/util')
 const Game = require('./src/Game')
-// const Round = require('./src/Round')
-// const Turn = require('./src/Turn')
-// const Deck = require('./src/Deck')
-// const Card = require('./src/Card')
-
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');  
-console.log('Node server running on port 3000');
+// console.log('Node server running on port 3000');
 const currentGame = new Game ()
 currentGame.start()
